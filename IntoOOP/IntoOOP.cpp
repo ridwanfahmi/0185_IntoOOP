edfb7f85919dec0a4c739abb36737f6802059b63
TOSHIBA <ridwanfahmidina12@gmail.com>
2024-04-23 10:07:36 +0700

    menampilkan nama, umur, dan jurusan mahasiswa

--- IntoOOP/IntoOOP.cpp
+++ IntoOOP/IntoOOP.cpp
@@ -8,7 +8,11 @@ public:   //akses modifier
 	string jurusan;
 
 	void output() {
-		
+		cout << "nama: " << nama << endl;
+		cout << "umur: " << umur << endl;
+		cout << "jurusan: " << jurusan << endl;
+	}
+
 };
 
 int main() {
